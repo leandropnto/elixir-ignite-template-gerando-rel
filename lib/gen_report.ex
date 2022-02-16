8cc3f6a184f10390c540fd3145f94406f1aa3a01
Leandro Pinto <leandro.pnto@gmail.com>
2022-02-16 14:10:56 -0300

    Concluindo os testes

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,63 @@
 defmodule GenReport do
+  alias GenReport.Parser
 
-  def build do
-    # TO DO
+  def build() do
+    {:error, "Insira o nome de um arquivo"}
+  end
+
+  def build(filename) do
+    lines =
+      filename
+      |> Parser.parse_file()
+
+    hours_per_name = sum_hours_per_name(lines)
+    hours_per_name_year = sum_hours_per_name_year(lines)
+    hours_per_name_year_month = sum_hours_per_name_month(lines)
+
+    %{
+      "all_hours" => hours_per_name,
+      "hours_per_month" => hours_per_name_year_month,
+      "hours_per_year" => hours_per_name_year
+    }
+  end
+
+  defp sum_hours_per_name(report) do
+    Enum.reduce(report, %{}, fn [name, horas, _dia, _mes, _ano], acc ->
+      Map.put(acc, name, (acc[name] || 0) + horas)
+    end)
+  end
+
+  defp sum_hours_per_name_year(data) do
+    Enum.reduce(data, %{}, fn [name, horas, _dia, _mes, ano], acc ->
+      Map.put(
+        acc,
+        name,
+        Map.put(acc[name] || %{ano => horas}, ano, (acc[name][ano] || 0) + horas)
+      )
+    end)
+  end
+
+  # defp sum_hours_per_name_year_month(data) do
+  #   Enum.reduce(data, %{}, fn [name, horas, _dia, mes, ano], acc ->
+  #     Map.put(
+  #       acc,
+  #       name,
+  #       Map.put(
+  #         acc[name] || %{ano => horas},
+  #         ano,
+  #         Map.put(acc[name][ano] || %{}, mes, (acc[name][ano][mes] || 0) + horas)
+  #       )
+  #     )
+  #   end)
+  # end
+
+  def sum_hours_per_name_month(data) do
+    Enum.reduce(data, %{}, fn [name, hours, _day, month, _year], acc ->
+      Map.put(
+        acc,
+        name,
+        Map.put(acc[name] || %{}, month, (acc[name][month] || 0) + hours)
+      )
+    end)
   end
 end
--- lib/parser.ex
+++ lib/parser.ex
@@ -1,5 +1,19 @@
 defmodule GenReport.Parser do
-  def call(filename) do
+  @months %{
+    1 => "janeiro",
+    2 => "fevereiro",
+    3 => "março",
+    4 => "abril",
+    5 => "maio",
+    6 => "junho",
+    7 => "julho",
+    8 => "agosto",
+    9 => "setembro",
+    10 => "outubro",
+    11 => "novembro",
+    12 => "dezembro"
+  }
+  def parse_file(filename) do
     filename
     |> File.stream!()
     |> Stream.map(&parse_line/1)
@@ -9,9 +23,18 @@ defmodule GenReport.Parser do
     line
     |> String.trim()
     |> String.split(",")
+    |> List.update_at(0, &String.downcase/1)
     |> List.update_at(1, &String.to_integer/1)
     |> List.update_at(2, &String.to_integer/1)
-    |> List.update_at(3, &String.to_integer/1)
+    |> List.update_at(3, &convert_to_month/1)
     |> List.update_at(4, &String.to_integer/1)
+  end
+
+  defp convert_to_month(str_number) do
+    number =
+      str_number
+      |> String.to_integer()
+
+    @months[number]
   end
 end
